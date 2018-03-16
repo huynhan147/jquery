bfa454a70a544b1c23c4c1151a14f5aa14a962a6
huynhan147 <huyitptit2015@gmail.com>
2018-03-16 11:32:12 +0700

    update

--- js/site2.js
+++ js/site2.js
@@ -1,4 +1,20 @@
        $(function(){
+            // var tmpx=$('ul li');
+            // let dem =0;
+            //  for(let i=0;i<tmpx.length;i++)
+            //         if($(tmpx[i]).attr('class')=='checked'){
+            //            dem =dem+1;    
+            //         // console.log($(ulli[i]).attr('class'));
+            //     }
+               
+            $(".clear").click(function(){
+                 ulli=$('ul li');
+                // console.log($(ulli[0]).attr('class'));
+                for(let i=0;i<ulli.length;i++)
+                    if($(ulli[i]).attr('class')=='checked'){
+                       $(ulli[i]).remove();     
+                }
+            });
             $(".add").click(function(){
                 $('#txtinput').show();
                 let textinput = $('#txtinput').val();
@@ -13,13 +29,39 @@
                         li.append(hr);          
                      $("#ul-list").append(li);
                 }
+                let dem=0;
+                ulli=$('ul li');
+                for(let i=0;i<ulli.length;i++)
+                    if($(ulli[i]).attr('class')!='checked'){
+                    dem =dem+1;       
+                }
+
+                 $('.dems').text(dem+ " Item");
+
             });
             $(document).on('click', 'span', function (e) {
                  // e.preventDefault();
                 $(this).parent().remove();
+                  let dem=0;
+                ulli=$('ul li');
+                for(let i=0;i<ulli.length;i++)
+                    if($(ulli[i]).attr('class')!='checked'){
+                    dem =dem+1;       
+                }
+
+                 $('.dems').text(dem+ " Item");
             });
              $(document).on('click', '#checkbox', function (e) {
                 $(this).parent().toggleClass('checked');
+                let dem=0;
+                ulli=$('ul li');
+                for(let i=0;i<ulli.length;i++)
+                    if($(ulli[i]).attr('class')!='checked'){
+                    dem =dem+1;       
+                }
+
+                 $('.dems').text(dem+ " Item");
+                 // console.log('xxxx');
 
             });
              $('.active').click(function(){
